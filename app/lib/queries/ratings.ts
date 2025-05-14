9fc6cf17f2bf726ed74fef5e29ea738faeb88748
Bailey Frye <baileyafrye@comcast.net>
2025-05-14 12:09:51 -0500

    Submitting ratings now working

--- app/lib/queries/ratings.ts
+++ app/lib/queries/ratings.ts
@@ -1,7 +1,9 @@
+import { getAuth } from "@clerk/tanstack-react-start/server";
 import axiosClient from "../axiosClient";
 import { createServerFn } from "@tanstack/react-start";
 import { toast } from "sonner";
 import { z } from "zod";
+import { getWebRequest } from "@tanstack/react-start/server";
 
 const submitRatingSchema = z.object({
   cocktailId: z.string(),
@@ -12,10 +14,23 @@ export const submitRating = createServerFn({ method: "POST" })
   .validator(submitRatingSchema)
   .handler(async ({ data }) => {
     const { cocktailId, rating } = data;
+    const { getToken } = await getAuth(getWebRequest()!);
 
-    await axiosClient.post(`ratings/${cocktailId}`, {
-      rating,
-    });
+    const token = await getToken({ template: "backend_api" });
+
+    const options = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    await axiosClient.post(
+      `ratings/${cocktailId}`,
+      {
+        rating,
+      },
+      options,
+    );
 
     toast.success("Successfully submitted rating");
   });
